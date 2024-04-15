bbce9d4f2c77577a6fe66e19dad33f40dcb19c09
Jason Hasselkus <jason.hasselkus@gmail.com>
2024-04-15 18:46:50 -0500

    updated forecast sorting

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -6,6 +6,11 @@ function Forecast({lat,lon}) {
     const [forecast,setData] = useState('')
     const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
     // api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid={API key}
+    const date = new Date()
+    const year = date.toLocaleString("default", { year: "numeric"})
+    const month = date.toLocaleString("default", { month: "2-digit"})
+    const day = date.toLocaleString("default", { day: "2-digit"})
+    const result = year + '-' + month + "-" + day;
 
     useEffect(() => {
         axios.get(forecastURL)
@@ -20,11 +25,25 @@ function Forecast({lat,lon}) {
     {forecast ? (
         <div>
                 <div>Forecast</div>
-                <ul>
-                    {forecast.list.map((item,idx) => (
-                      <li key={idx}>{item.dt_txt} / {item.main.temp} / Min: {item.main.temp_min.toFixed()} / Max: {item.main.temp_max.toFixed()}</li>
-                      ))
+                <div>
+                  <h3 className='font-bold'>Next 3 Hours</h3>
+                  {forecast.list.map((wthr,cnt) =>
+                  {
+                    const dd = wthr.dt_txt.split(" ")
+                    console.log("DX: ",dd[0])
+                    if(result === dd[0]) {
+                      console.log("Today: ", cnt)
                     }
+                  })
+                }
+                </div>
+                <ul>
+                    {forecast.list.map((item,idx) => 
+                     
+                        (
+                          <li key={idx}>{item.dt_txt} / {item.main.temp} / Min: {item.main.temp_min.toFixed()} / Max: {item.main.temp_max.toFixed()}</li>
+                        )
+                    )}   
                 </ul>
         </div>
     ):
